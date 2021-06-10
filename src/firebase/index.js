158f828241d65502961893445bce91fc7c032dd7
hieupv <hieunucek6@gmail.om>
2021-06-10 09:43:47 +0700

    update

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,12 +1,8 @@
 import firebase from "firebase/app";
 import "firebase/storage";
 const firebaseConfig = {
-    apiKey: "AIzaSyCmHg0Tx3LjHMQ8MTMbNqxHIBG2hQ0WeKI",
-    authDomain: "ecma-a7973.firebaseapp.com",
-    projectId: "ecma-a7973",
-    storageBucket: "ecma-a7973.appspot.com",
-    messagingSenderId: "59741284933",
-    appId: "1:59741284933:web:9c557b392aa82cbf2d512c"
+    // Ban them config vao day
+
 
 };
 
